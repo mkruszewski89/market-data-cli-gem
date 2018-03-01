5acda3bc31942133d0c2083d81b26f6efc82b606
github username <github email address>
2018-03-01 12:37:45 -0500

    create MarketData::CLI#call loop and #display with dummy data

--- lib/market_data/cli.rb
+++ lib/market_data/cli.rb
@@ -1,7 +1,19 @@
 class MarketData::CLI
 
   def call
-    puts "Enter a ticker symbol:"
+    input = nil
+    while input != 'exit'
+      puts "Enter a ticker symbol or 'exit':"
+      input = gets.strip.downcase
+      display(input)
+    end
+    puts "Goodbye!"
+  end
+
+  def display(input)
+    puts <<-DOC
+      Ticker | Full_Name | Price | 1-day % | 3-month % | 1-year %
+    DOC
   end
 
 end
